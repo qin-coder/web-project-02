2d83237d02cfc993074d6c4cd73a9fceb15e6ab5
Xuwei <qinxuwei15@gmail.com>
2025-08-11 17:46:04 +0200

    tlias log config

--- tlias-web-management/src/main/java/com/ite/controller/DeptController.java
+++ tlias-web-management/src/main/java/com/ite/controller/DeptController.java
@@ -3,24 +3,32 @@ package com.ite.controller;
 import com.ite.pojo.Dept;
 import com.ite.pojo.Result;
 import com.ite.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 //调用service层，响应结果
+@Slf4j
 @RestController
 @RequestMapping("/depts")
 public class DeptController {
+
     @Autowired
     private DeptService deptService;
 
     //查询所有部门数据
     @GetMapping
     public Result list() {
-        System.out.println("查询全部门数据");
+        //System.out.println("查询全部门数据");
+        log.info("查询全部门数据");
         List<Dept> deptList = deptService.findAll();
         return Result.success(deptList);
     }
+
     //删除部门数据
     @DeleteMapping
 //    public Result delete(HttpServletRequest  request) {
@@ -30,23 +38,28 @@ public class DeptController {
 //        return Result.success();
 //
 //    }
-    public Result delete( Integer id) {
-        System.out.println("删除部门数据" +  id);
+    public Result delete(Integer id) {
+        log.info("删除部门数据: {}", id);
+        //System.out.println("删除部门数据" + id);
         deptService.deleteById(id);
         return Result.success();
 
     }
+
     /*添加部门*/
     @PostMapping
     public Result add(@RequestBody Dept dept) {
-        System.out.println("添加部门数据" +  dept);
+        log.info("添加部门数据:{}", dept);
+        //System.out.println("添加部门数据" + dept);
         deptService.add(dept);
         return Result.success();
     }
+
     /*根据id查询部门*/
     @GetMapping("{id}")
     public Result getInfo(@PathVariable Integer id) {
-        System.out.println("查询部门数据" +  id);
+        log.info("查询部门数据 : {}" , id);
+        //System.out.println("查询部门数据" + id);
         Dept dept = deptService.getById(id);
         return Result.success(dept);
     }
@@ -54,7 +67,8 @@ public class DeptController {
     /*修改部门*/
     @PutMapping
     public Result update(@RequestBody Dept dept) {
-        System.out.println("修改部门数据" +  dept);
+        log.info("修改部门数据: {}", dept);
+        //System.out.println("修改部门数据" + dept);
         deptService.updateDept(dept);
         return Result.success();
     }
