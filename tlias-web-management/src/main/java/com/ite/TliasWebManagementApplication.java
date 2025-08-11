4e68a021d41495a16073a1211ab288810f705875
Xuwei <qinxuwei15@gmail.com>
2025-08-11 12:41:11 +0200

    tlias init

---
+++ tlias-web-management/src/main/java/com/ite/TliasWebManagementApplication.java
@@ -0,0 +1,14 @@
+package com.ite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasWebManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliasWebManagementApplication.class,
+                args);
+    }
+
+}
