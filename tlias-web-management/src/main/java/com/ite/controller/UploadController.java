d6cef760e6a9bd93b2f2e9313a76384bb2c34225
Xuwei <qinxuwei15@gmail.com>
2025-08-14 12:08:32 +0200

    tlias file update store locally with unique name

--- tlias-web-management/src/main/java/com/ite/controller/UploadController.java
+++ tlias-web-management/src/main/java/com/ite/controller/UploadController.java
@@ -6,13 +6,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 @Slf4j
 @RestController
 public class UploadController {
     @PostMapping("/upload")
     public Result upload(String name, Integer age,
-                         MultipartFile file) {
+                         MultipartFile file) throws Exception {
         log.info("接受参数: {},{},{}", name, age, file);
+        //获取原始文件名
+        String originalFilename = file.getOriginalFilename();
+        //新的文件名
+        String extension =
+                originalFilename.substring(originalFilename.lastIndexOf("."));
+        String newFileName = UUID.randomUUID().toString() + extension;
+        file.transferTo(new File("D:/projectFiles/" + newFileName));
         return Result.success();
     }
 }
